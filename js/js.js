6e3ef71698c874bf14acd74db1fa1cc9bd5357ad
YuriChlek <yurichlek@gmail.com>
2019-09-25 22:25:09 +0300

    Slider was added

--- js/js.js
+++ js/js.js
@@ -22,4 +22,41 @@
         });
     }
 
+    $('.layer-wrapper').slick({
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        prevArrow: 0,//дописати
+        nextArrow: 0,//дописати
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+
 })();
